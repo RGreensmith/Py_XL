0c927be633cdf43155a442b51d315b0a704402af
RGreensmith <rose.greensmith@yahoo.co.uk>
2022-06-24 14:38:29 +0100

    split code into functions, and renamed some files

---
+++ oSCxlsx.py
@@ -0,0 +1,31 @@
+import openpyxl
+from pathlib import Path
+######## notes #########
+# loading values only
+#wb = openpyxl.load_workbook(filename, data_only=True)
+
+#Read active sheet
+#sheet = wb_obj.active
+
+########################
+def oSCxlsx(openSaveClose = ["open","save", "saveClose","close"],wbName = ["wb"],
+    wbPath = ["to_ignore"],wbNm = ["BNG_no_macros_3_1.xlsx"]):
+    filePath = [wbPath,wbName]
+
+    if openSaveClose == "open":
+
+        # Setting the path to the xlsx file (BNG calculator v3.1) and Read specific sheet (in this case A-2 Site Habitat Creation)
+        xlsx_file = Path(wbPath,wbNm)
+        wb = openpyxl.load_workbook(xlsx_file)
+        
+        return wb
+
+    elif openSaveClose == "save":
+        wb.save(filePath)
+
+    elif openSaveClose == "saveClose":
+        wb.save(filePath)
+        wb.close(wb)
+
+    elif openSaveClose == "close":
+        wb.close(wb)
--- testingMakingConnections.py
+++ testingMakingConnections.py
@@ -1,25 +1,8 @@
-import openpyxl
 import pandas as pd
-from pathlib import Path
 
-# Setting the path to the xlsx file
-xlsx_file = Path('to_ignore','test_xlsx.xlsx')
-#print(xlsx_file)
+habTypes = pd.read_csv('to_ignore/habitat_types') # habitat lists to loop through
+save_path = 'to_ignore'
 
-wb_obj = openpyxl.load_workbook(xlsx_file)
-#print(wb_obj)
-
-#Read active sheet
-sheet = wb_obj.active
-#print(sheet)
-
-print(sheet["B2"].value)
-
-#populate pandas df with content of test_xlsx
-df = pd.DataFrame(columns=['A','B','C'],index=[1,2,3])
-df.loc['1','A'] = sheet["B2"].value
-
-print(df)
-df.to_excel("to_ignore/df.xlsx")
+py_xl_BNG(habTypes,save_path)
 
 ### End ###
